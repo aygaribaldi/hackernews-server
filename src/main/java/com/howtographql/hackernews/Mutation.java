2bf5dc8ad1bc028d62718c787477359f26ebcf1e
Alexandra Garibaldi <aygaribaldi@hotmail.com>
2020-05-17 18:00:44 -0400

    other files

---
+++ src/main/java/com/howtographql/hackernews/Mutation.java
@@ -0,0 +1,47 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import graphql.GraphQLException;
+import graphql.schema.DataFetchingEnvironment;
+
+public class Mutation implements GraphQLRootResolver {
+
+    private final LinkRepository linkRepository;
+    private final UserRepository userRepository;
+    private final VoteRepository voteRepository;
+
+    public Mutation(LinkRepository linkRepository, UserRepository userRepository, VoteRepository voteRepository) {
+        this.linkRepository = linkRepository;
+        this.userRepository = userRepository;
+        this.voteRepository = voteRepository;
+    }
+
+    // return linkRepository.saveLink(newLink);
+    public Link createLink(String url, String description, DataFetchingEnvironment env) {
+        AuthContext context = env.getContext();
+        Link newLink = new Link(url, description, context.getUser().getId());
+        linkRepository.saveLink(newLink);
+        return newLink;
+    }
+
+    public User createUser(String name, AuthData auth) {
+        User newUser = new User(name, auth.getEmail(), auth.getPassword());
+        return userRepository.saveUser(newUser);
+    }
+
+    public SigninPayload signinUser(AuthData auth) throws IllegalAccessException {
+        User user = userRepository.findByEmail(auth.getEmail());
+        if (user.getPassword().equals(auth.getPassword())) {
+            return new SigninPayload(user.getId(), user);
+        }
+        throw new GraphQLException("Invalid credentials");
+    }
+
+    public Vote createVote(String linkId, String userId) {
+        return voteRepository.saveVote(new Vote(Instant.now().atZone(ZoneOffset.UTC), userId, linkId));
+    }
+}
